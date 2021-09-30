587bf98856da6c3942aed637e2181d898bc83344
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2021-09-30 22:35:10 +0200

    Added image hover to the desktop version to the CSM page. Added text and interaction with buttons. It was not like I would like, but it's working.

--- js/script.js
+++ js/script.js
@@ -16,3 +16,28 @@ function projectExamAccess() {
     window.open("https://pkderlam.one/ProjectExam", "ProjectExam", "width=600,height=400")
 }
 projectExam.addEventListener("click", projectExamAccess);
+
+
+
+const titleReport = document.querySelectorAll("h3");
+const textReport = document.querySelectorAll(".text_report");
+const closeText = document.querySelectorAll(".closeText");
+const arrow = document.querySelectorAll(".fa-caret-square-down");
+let count = 0;
+for (let i = 0; i < titleReport.length; i++) {
+    titleReport[i].addEventListener("click", function textAppear(event) {
+        textReport[i].style.display = "block";
+        arrow[i].style.rotate = "180deg";
+        count++
+        if (count % 2 == 0) {
+            textReport[i].style.display = "none";
+            arrow[i].style.rotate = "360deg";
+        }
+    });
+    if (typeof closeText[i] !== `undefined`) {
+        closeText[i].addEventListener("click", function closeTextReport(event) {
+            textReport[i].style.display = "none";
+            arrow[i].style.rotate = "360deg";
+        });
+    }
+}
