ec8d33828c23f84a20390cfa93ba8a3840e8d93d
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2021-10-05 14:58:29 +0200

    Added text to Rainydays report, Rainydays' image to hover, change the icon beside the title when the report is open

--- js/script.js
+++ js/script.js
@@ -22,23 +22,22 @@ projectExam.addEventListener("click", projectExamAccess);
 const titleReport = document.querySelectorAll("h3");
 const textReport = document.querySelectorAll(".text_report");
 const closeText = document.querySelectorAll(".closeText");
-const plus = document.querySelectorAll("i");
+const moreLess = document.querySelectorAll(".more_less");
 let count = 0;
 for (let i = 0; i < titleReport.length; i++) {
     titleReport[i].addEventListener("click", function textAppear(event) {
         textReport[i].style.display = "block";
         count++
-        console.log(plus[i]);
-        plus[i].style.display = "none";
+        moreLess[i].innerHTML = `<i class="far fa-minus-square"></i>`;
         if (count % 2 == 0) {
             textReport[i].style.display = "none";
-            plus[i].style.display = "initial";
+            moreLess[i].innerHTML = `<i class="far fa-plus-square"></i>`;
         }
     });
     if (typeof closeText[i] !== `undefined`) {
         closeText[i].addEventListener("click", function closeTextReport(event) {
             textReport[i].style.display = "none";
-            plus[i].style.display = "initial";
+            moreLess[i].innerHTML = `<i class="far fa-plus-square"></i>`;
         });
     }
 }
